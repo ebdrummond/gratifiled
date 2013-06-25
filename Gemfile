fa78e27857a8f102b3c58c30ff314d2fdbab8fa1
erindrummond <e.b.drummond@gmail.com>
2013-06-25 15:30:58 -0600

    initial gems installed, pg dbs created, rspec ready to go

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'jquery-rails'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.0'
+  gem 'capybara'
+  gem 'simplecov'
+  gem 'pry'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'dotenv-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gratifiled::Application.config.secret_token = 'e2b765cf5fd14afebad6c29b6826675d12365da7a989d209fa544237c102327e4bfde789084fdc5eac994f1d28b40338eaa0f2d0da6456f9f08da5aa43cdb358'
