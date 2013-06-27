cebd24989d5874fdd3fbc7a6a95dc30a91be117a
erindrummond <e.b.drummond@gmail.com>
2013-06-27 10:01:56 -0600

    tried testing email worker; having trouble, so left as pending.  Got coverage up on emailer.rb

--- spec/features/user_uploads_file_spec.rb
+++ spec/features/user_uploads_file_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe "a user sends an email to recipient" do
-  def create_document
+  def create_valid_document
     visit new_document_path
     fill_in("document[name]", :with => "erin")
     fill_in("document[email]", :with => "erin@example.com")
     fill_in("document[recipient_email]", :with => "brock@example.com")
     fill_in("document[message]", :with => "the dog ate my homework")
-    expect{ click_button("Send document") }.to change(Document, :count).by(1)
   end
 
   it "has a button to add a new file" do
@@ -30,24 +29,42 @@ describe "a user sends an email to recipient" do
     expect(page).to have_button("Send document")
   end
 
-  it "creates a new document instance" do
-    create_document
+  context "with valid input to create the document" do
+    it "creates a new document instance" do
+      create_valid_document
+      expect{ click_button("Send document") }.to change(Document, :count).by(1)
+    end
+
+    it "redirects to a confirmation page on document creation" do
+      create_valid_document
+      click_button("Send document")
+      expect(current_path).to eq(document_confirmation_path(Document.last.id))
+    end
+
+    it "includes confirmation details of the created document" do
+      create_valid_document
+      click_button("Send document")
+      document = Document.last
+      expiration = (document.created_at + 3.days).to_time.localtime
+      formatted_expiration = expiration.strftime("%B %-d at %-l:%M %P")
+      expect(page).to have_content("erin")
+      expect(page).to have_content("erin@example.com")
+      expect(page).to have_content("brock@example.com")
+      expect(page).to have_content("the dog ate my homework")
+      expect(page).to have_content(formatted_expiration)
+    end
   end
 
-  it "redirects to a confirmation page on document creation" do
-    create_document
-    expect(current_path).to eq(document_confirmation_path(Document.last.id))
-  end
-
-  it "includes confirmation details of the created document" do
-    create_document
-    document = Document.last
-    expiration = (document.created_at + 3.days).to_time.localtime
-    formatted_expiration = expiration.strftime("%B %-d at %-l:%M %P")
-    expect(page).to have_content("erin")
-    expect(page).to have_content("erin@example.com")
-    expect(page).to have_content("brock@example.com")
-    expect(page).to have_content("the dog ate my homework")
-    expect(page).to have_content(formatted_expiration)
+  context "with invalid input to create the document" do
+    it "does not create the new document" do
+      visit new_document_path
+      fill_in("document[name]", :with => "")
+      fill_in("document[email]", :with => "erin@example.com")
+      fill_in("document[recipient_email]", :with => "brock@example.com")
+      fill_in("document[message]", :with => "the dog ate my homework")
+      click_button("Send document")
+      expect(current_path).to eq(documents_path)
+      expect(page).to have_content("Name can't be blank")
+    end
   end
 end
---
+++ spec/mailers/emailer_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Emailer do
+  describe "#send_document_email" do
+    let!(:document) {Document.create(name: "erin",
+                                    email: "erin@sample.com",
+                                    recipient_email: "brock@sample.com")}
+    let!(:worker) {SendEmailsWorker.new}
+    let!(:mail) {Emailer.send_document_email(document)}
+
+    xit "receives the information from the Send Emails Worker" do
+      Emailer.should_receive(:send_document_email).with(document)
+      worker.perform(document.id)
+    end
+
+    it "renders the headers" do
+      mail.subject.should eq("erin has shared a file with you!")
+      mail.to.should eq(["brock@sample.com"])
+      mail.from.should eq(["gratifiled@gmail.com"])
+    end
+
+    it "renders the body" do
+      mail.body.should include(document.formatted_expiration)
+    end
+  end
+end
